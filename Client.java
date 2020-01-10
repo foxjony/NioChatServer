b499c61840b9249ac37a0ddffbbf4bdd05a74c95
foxjony <37145483+foxjony@users.noreply.github.com>
2020-01-10 08:38:06 +0200

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,33 @@
+// TCP Client (Port 4040)
+
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client {
+	public static void main(String[] args) {
+		try (
+			Socket socket = new Socket("192.168.0.5", 4040);
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		) {
+			String inputLine;
+			out.println(1);
+			System.out.println("Send: 1");
+
+			while ((inputLine = in.readLine()) != null) {
+				System.out.println("Read: "+inputLine);
+				int number = Integer.valueOf(inputLine);
+				if (number >= 10) break;
+				number++;
+				out.println(number);
+				System.out.println("Send: "+number);
+				//Thread.sleep(2000);
+			}
+			System.out.println("Disconnected");
+		} catch (Throwable e) {
+			System.out.println("Error: "+e.getMessage());
+		}
+	}
+}
