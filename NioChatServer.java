22d12d9496e387bb369c30b9f8d1f9dfbf9fca8e
foxjony <37145483+foxjony@users.noreply.github.com>
2020-02-10 00:42:24 +0200

    Update NioChatServer.java

--- NioChatServer.java
+++ NioChatServer.java
@@ -1,4 +1,7 @@
-// NIO Server - https://github.com/foxjony/nioserver
+// TCP NIO Chat Server (Port 3000) - https://github.com/foxjony/nioserver
+
+// REG~1~2
+// SMS~1~2~text
 
 import java.io.IOException;
 import java.net.Socket;
@@ -14,7 +17,7 @@ public class NioChatServer implements Runnable {
 	private int clients;
 	private ServerSocketChannel ssc;
 	private Selector selector;
-	private ByteBuffer buf = ByteBuffer.allocate(32);	// (256)
+	private ByteBuffer buf = ByteBuffer.allocate(256);
 	//private long tim_start = System.currentTimeMillis();
 
 	NioChatServer(int port) throws IOException {
@@ -31,7 +34,7 @@ public class NioChatServer implements Runnable {
 			byte[] ip = new byte[4];
 			ip = InetAddress.getLocalHost().getAddress();
 			String localAddress = (0xff&(int)ip[0])+"."+(0xff&(int)ip[1])+"."+(0xff&(int)ip[2])+"."+(0xff&(int)ip[3]);
-			System.out.println("Run NIO Server "+localAddress+":"+this.port);
+			System.out.println("Started NIO Chat Server "+localAddress+" port "+this.port);
 			Iterator<SelectionKey> iter;
 			SelectionKey key;
 
@@ -52,7 +55,22 @@ public class NioChatServer implements Runnable {
 		}
 	}
 
-	private final ByteBuffer welcomeBuf = ByteBuffer.wrap("Welcome!\n".getBytes());
+	public boolean isNum(String str) {
+		if (str == null || str.length() == 0) return false;
+		int i = 0;
+		//if (str.charAt(0) == '-') {
+		//  if (str.length() == 1) return false;
+		//  i = 1;
+		//}
+		char c;
+		for (; i<str.length(); i++) {
+			c = str.charAt(i);
+			if (!(c >= '0' && c <= '9')) return false;
+		}
+		return true;
+	}
+
+	//private final ByteBuffer welcomeBuf = ByteBuffer.wrap("Welcome!\n".getBytes());
 
 	private void handleAccept(SelectionKey key) throws IOException {
 		SocketChannel sc = ((ServerSocketChannel) key.channel()).accept();
@@ -61,8 +79,8 @@ public class NioChatServer implements Runnable {
 			.append(sc.socket().getPort()).toString();
 		sc.configureBlocking(false);
 		sc.register(selector, SelectionKey.OP_READ, address);
-		sc.write(welcomeBuf);
-		welcomeBuf.rewind();
+		//sc.write(welcomeBuf);
+		//welcomeBuf.rewind();
 		clients++;
 		long tim = System.currentTimeMillis();
 		System.out.println(tim+address+" Connected ("+clients+")");
@@ -95,6 +113,51 @@ public class NioChatServer implements Runnable {
 			ch.close();
 		} else {
 			msg = key.attachment()+" => "+sb.toString();
+			if (msg.indexOf("REG~") == 0) {
+				// Register Client
+				String[] subStr = msg.split("~", 3);	// "REG~1~2"
+				if (subStr.length == 3) {
+					// city, st, uid, order
+					int s = 0;
+					if (isNum(subStr[1])) s++;
+					if (isNum(subStr[2])) s++;
+					if (s == 2) {
+						/*
+						list[clients] = new Id(key.attachment(), 
+							Integer.parseInt(subStr[1]), 
+							Integer.parseInt(subStr[2]));
+						*/
+						msg = key.attachment()+" => REG OK";
+					} else {
+						msg = key.attachment()+" ==> "+msg;
+					}
+				} else {
+					msg = key.attachment()+" => "+msg;
+				}
+			} if (msg.indexOf("SMS~") == 0) {
+				// Message to Client
+				String[] subStr = msg.split("~", 4);	// "REG~1~2~text"
+				if (subStr.length == 4) {
+					// city, st, uid, order
+					int s = 0;
+					if (isNum(subStr[1])) s++;
+					if (isNum(subStr[2])) s++;
+					if (s == 2) {
+						/*
+						list[clients] = new Id(key.attachment(), 
+							Integer.parseInt(subStr[1]), 
+							Integer.parseInt(subStr[2]));
+						*/
+						msg = key.attachment()+" => SMS OK";
+					} else {
+						msg = key.attachment()+" ==> "+msg;
+					}
+				} else {
+					msg = key.attachment()+" => "+msg;
+				}
+			} else {
+				msg = key.attachment()+" => "+msg;
+			}
 		}
 
 		long tim = System.currentTimeMillis();
@@ -116,8 +179,27 @@ public class NioChatServer implements Runnable {
 	}
 
 	public static void main(String[] args) throws IOException {
-		int localPort = Integer.parseInt(args[0]);
+		int localPort = 3000;
+		if (args.length == 1) localPort = Integer.parseInt(args[0]);
 		NioChatServer server = new NioChatServer(localPort);
 		(new Thread(server)).start();
 	}
+}
+
+class Id {
+	public Object addr;
+	public int city;
+	public int st;
+	public int uid;
+	public int order;
+
+	/*
+	public Id (int city, int st, int uid, int order) {
+		//this.addr = addr;
+		this.city = city;
+		this.st = st;
+		this.uid = uid;
+		this.order = order;
+	}
+	*/
 }
