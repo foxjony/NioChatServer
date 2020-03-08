99cd74621a2bf6c637f6af880c839c0b9991edd2
foxjony <37145483+foxjony@users.noreply.github.com>
2020-03-08 17:07:46 +0200

    Add files via upload

--- NioChatServer.java
+++ NioChatServer.java
@@ -1,7 +1,9 @@
-// TCP NIO Chat Server (Port 3000) - https://github.com/foxjony/nioserver
+// TCP NIO Chat Server (Port 3000) (8.03.2020)
+// https://github.com/foxjony/NioChatServer
+// https://www.codeflow.site/ru/article/java-nio-selector
 
-// REG~1~2
-// SMS~1~2~text
+// M~13~3336~6~1~0~12~555~R 		(M, obl, city, st, typ,   0, id, order, msg) Register
+// M~13~3336~6~1~13~12~555~text 	(M, obl, city, st, typ, uid, id, order, msg) Message
 
 import java.io.IOException;
 import java.net.Socket;
@@ -19,6 +21,7 @@ public class NioChatServer implements Runnable {
 	private Selector selector;
 	private ByteBuffer buf = ByteBuffer.allocate(256);
 	//private long tim_start = System.currentTimeMillis();
+	//private final ByteBuffer welcomeBuf = ByteBuffer.wrap("Welcome!\n".getBytes());
 
 	NioChatServer(int port) throws IOException {
 		this.port = port;
@@ -41,7 +44,6 @@ public class NioChatServer implements Runnable {
 			while (this.ssc.isOpen()) {
 				selector.select();
 				iter = this.selector.selectedKeys().iterator();
-
 				while (iter.hasNext()) {
 					key = iter.next();
 					iter.remove();
@@ -58,10 +60,6 @@ public class NioChatServer implements Runnable {
 	public boolean isNum(String str) {
 		if (str == null || str.length() == 0) return false;
 		int i = 0;
-		//if (str.charAt(0) == '-') {
-		//  if (str.length() == 1) return false;
-		//  i = 1;
-		//}
 		char c;
 		for (; i<str.length(); i++) {
 			c = str.charAt(i);
@@ -70,27 +68,25 @@ public class NioChatServer implements Runnable {
 		return true;
 	}
 
-	//private final ByteBuffer welcomeBuf = ByteBuffer.wrap("Welcome!\n".getBytes());
-
 	private void handleAccept(SelectionKey key) throws IOException {
 		SocketChannel sc = ((ServerSocketChannel) key.channel()).accept();
-		String address = (new StringBuilder(sc.socket().getInetAddress().toString()))
-			.append(":")
-			.append(sc.socket().getPort()).toString();
+		//String address = (new StringBuilder(sc.socket().getInetAddress().toString())).append(":").append(sc.socket().getPort()).toString();
 		sc.configureBlocking(false);
-		sc.register(selector, SelectionKey.OP_READ, address);
+		sc.register(selector, SelectionKey.OP_READ, "0~0~0~0~0");	// "obl~city~st~typ~id"
 		//sc.write(welcomeBuf);
 		//welcomeBuf.rewind();
 		clients++;
 		long tim = System.currentTimeMillis();
-		System.out.println(tim+address+" Connected ("+clients+")");
-		broadcast(tim+address+" Connected ("+clients+")\n");
+		//System.out.println(tim+": "+address+" Connected ("+clients+")");
+		System.out.println(tim+": Connected ("+clients+")");
+		//broadcast(0, 0, 0, 0, 0, 0, 0, tim+": "+address+" Connected ("+clients+")\n");
+		//broadcast(0, tim+": "+address+" Connected ("+clients+")\n");
 	}
 
 	private void handleRead(SelectionKey key) throws IOException {
 		SocketChannel ch = (SocketChannel) key.channel();
 		StringBuilder sb = new StringBuilder();
-		String msg;
+		long tim;
 		int read = 0;
 		buf.clear();
 
@@ -109,71 +105,79 @@ public class NioChatServer implements Runnable {
 
 		if ((sb.length() == 0) || (read == -1)) {
 			if (clients > 0) clients--;
-			msg = key.attachment()+" Disconnected ("+clients+")";
+			tim = System.currentTimeMillis();
+			System.out.println(tim+": Disconnected ("+clients+")");
 			ch.close();
 		} else {
-			msg = key.attachment()+" => "+sb.toString();
-			if (msg.indexOf("REG~") == 0) {
-				// Register Client
-				String[] subStr = msg.split("~", 3);	// "REG~1~2"
-				if (subStr.length == 3) {
-					// city, st, uid, order
+			if (sb.toString().indexOf("M~") == 0) {
+				// "M~13~3336~6~1~13~12~555~text" (M, obl, city, st, typ, uid, id, order, msg)
+				String[] subStr = sb.toString().split("~", 9);
+				if (subStr.length == 9) {
 					int s = 0;
 					if (isNum(subStr[1])) s++;
 					if (isNum(subStr[2])) s++;
-					if (s == 2) {
-						/*
-						list[clients] = new Id(key.attachment(), 
-							Integer.parseInt(subStr[1]), 
-							Integer.parseInt(subStr[2]));
-						*/
-						msg = key.attachment()+" => REG OK";
-					} else {
-						msg = key.attachment()+" ==> "+msg;
+					if (isNum(subStr[3])) s++;
+					if (isNum(subStr[4])) s++;
+					if (isNum(subStr[5])) s++;
+					if (isNum(subStr[6])) s++;
+					if (isNum(subStr[7])) s++;
+					if (s == 7) {
+						int obl = Integer.parseInt(subStr[1]);
+						int city = Integer.parseInt(subStr[2]);
+						int st = Integer.parseInt(subStr[3]);
+						int typ = Integer.parseInt(subStr[4]);
+						int uid = Integer.parseInt(subStr[5]);
+						int id = Integer.parseInt(subStr[6]);
+						int order = Integer.parseInt(subStr[7]);
+						//String msg = subStr[8];
+						//key.attach(subStr[6]);
+						if (order == 0) {
+							// "obl~city~st~typ~id"
+							key.attach(subStr[1]+"~"+subStr[2]+"~"+subStr[3]+"~"+subStr[4]+"~"+subStr[6]);
+							broadcast(0, 0, id, "ok\n");
+						} else {
+							//broadcast(obl, typ, uid, msg+"\n");
+							broadcast(obl, typ, uid, sb+"\n");
+						}
+						tim = System.currentTimeMillis();
+						System.out.println(tim+": "+sb);
 					}
-				} else {
-					msg = key.attachment()+" => "+msg;
 				}
-			} if (msg.indexOf("SMS~") == 0) {
-				// Message to Client
-				String[] subStr = msg.split("~", 4);	// "REG~1~2~text"
-				if (subStr.length == 4) {
-					// city, st, uid, order
-					int s = 0;
-					if (isNum(subStr[1])) s++;
-					if (isNum(subStr[2])) s++;
-					if (s == 2) {
-						/*
-						list[clients] = new Id(key.attachment(), 
-							Integer.parseInt(subStr[1]), 
-							Integer.parseInt(subStr[2]));
-						*/
-						msg = key.attachment()+" => SMS OK";
-					} else {
-						msg = key.attachment()+" ==> "+msg;
-					}
-				} else {
-					msg = key.attachment()+" => "+msg;
-				}
-			} else {
-				msg = key.attachment()+" => "+msg;
 			}
 		}
-
-		long tim = System.currentTimeMillis();
-		System.out.println(tim+msg);
-		broadcast(tim+msg+"\n");
 	}
 
-	private void broadcast(String msg) throws IOException {
+	// broadcast(int obl, int city, int st, int typ, int uid, int id, int order, String msg)
+	private void broadcast(int obl, int typ, int uid, String msg) throws IOException {
 		ByteBuffer msgBuf = ByteBuffer.wrap(msg.getBytes());
-		int i = 0;
+		String user;
+		int obl2, typ2, id2;	// city2, st2
+		//int id;
 		for (SelectionKey key : selector.keys()) {
 			if (key.isValid() && key.channel() instanceof SocketChannel) {
 				SocketChannel sch = (SocketChannel) key.channel();
-				sch.write(msgBuf);
-				msgBuf.rewind();
-				i++;
+				//id = Integer.parseInt(key.attachment().toString());
+				user = key.attachment().toString();
+				String[] data = user.toString().split("~", 5);	// "obl~city~st~typ~id"
+				if (data.length == 5) {
+					obl2 = Integer.parseInt(data[0]);
+					//city2 = Integer.parseInt(data[1]);
+					//st2 = Integer.parseInt(data[2]);
+					typ2 = Integer.parseInt(data[3]);
+					id2 = Integer.parseInt(data[4]);
+					if (uid != 0) {
+						if (uid == id2) {
+							sch.write(msgBuf);
+							msgBuf.rewind();
+							break;
+						}
+					} else {
+						if ((obl == obl2) && (typ == typ2)) {
+							sch.write(msgBuf);
+							msgBuf.rewind();
+						}
+					}
+				}
 			}
 		}
 	}
@@ -184,22 +188,4 @@ public class NioChatServer implements Runnable {
 		NioChatServer server = new NioChatServer(localPort);
 		(new Thread(server)).start();
 	}
-}
-
-class Id {
-	public Object addr;
-	public int city;
-	public int st;
-	public int uid;
-	public int order;
-
-	/*
-	public Id (int city, int st, int uid, int order) {
-		//this.addr = addr;
-		this.city = city;
-		this.st = st;
-		this.uid = uid;
-		this.order = order;
-	}
-	*/
 }
